c0e2f78295153036a8ea7ec6db90ddea96c349e3
Teppei Yoshida New Mac <teppei@njc.co.jp>
2025-09-19 14:59:12 +0900

    20250919_01

--- src/database/migrations/2025_09_19_144652_create_products_table.php
+++ src/database/migrations/2025_09_19_144652_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price')->unsigned();
+            $table->integer('category_id')->unsigned();
             $table->timestamps();
         });
     }
